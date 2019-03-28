b096aac00086f1e2591e83d34dc8f1ef7c23686f
zhaozongchang <xingyingsuifeng@gmail.com>
2019-03-28 14:23:47 +0800

    add payment_method function

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,11 +5,19 @@ class Order < ApplicationRecord
 
   def generate_token
     self.token = SecureRandom.uuid
-  end 
+  end
 
   validates :billing_name, presence: true
   validates :billing_address, presence: true
   validates :shipping_name, presence: true
   validates :shipping_address, presence: true
+
+  def set_payment_with!(method)
+    self.update_columns(payment_method: method)
+  end
+
+  def pay!
+    self.update_columns(is_paid: true)
+  end 
 
 end
